508d0afd99891cc1ae77d1ec9b0add54cd59ea8c
Turovae <ekaterina.turova@mail.ru>
2023-05-15 18:10:25 +0700

    add build setting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(( {command} ) => {
+  console.log(command);
+
+  const setting = {
+    plugins: [react()],
+  };
+
+  if (command === 'build') setting.base = 'https://turovae.github.io/ra16-events-state-store/'
+
+  return setting;
+});
